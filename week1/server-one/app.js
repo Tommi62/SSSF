62bce1b53d11f56e4a9f4284b336ab6463976bc7
Tommi62 <tommi.vainio21@gmail.com>
2022-03-16 16:01:32 +0200

    Second commit

---
+++ week1/server-one/app.js
@@ -0,0 +1,22 @@
+'use strict';
+import express from 'express'
+const app = express()
+
+app.use(express.static('public'));
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/hello', function (req, res) {
+    res.send('hello world')
+});
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+        name: 'Frank',
+        birthdate: '2010-12-25',
+        weight: 5,
+    };
+    res.json(cat);
+});
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
